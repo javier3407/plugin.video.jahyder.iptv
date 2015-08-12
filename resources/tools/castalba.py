376c41aaba69e48b42cdcf0c567752832f2a2459
javier3407 <junalerio@gmail.com>
2015-08-12 11:39:55 -0300

    suvir archibos

---
+++ resources/tools/castalba.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# PalcoTV Regex de castalba
+# Version 0.1 (17.10.2014)
+#------------------------------------------------------------
+# License: GPL (http://www.gnu.org/licenses/gpl-3.0.html)
+# Gracias a la librería plugintools de Jesús (www.mimediacenter.info)
+
+
+import os
+import sys
+import urllib
+import urllib2
+import re
+import shutil
+import zipfile
+import time
+
+import xbmc
+import xbmcgui
+import xbmcaddon
+import xbmcplugin
+
+import plugintools
+import json
+
+
+home = xbmc.translatePath(os.path.join('special://home/addons/plugin.video.palcotv/', ''))
+tools = xbmc.translatePath(os.path.join('special://home/addons/plugin.video.palcotv/resources/tools', ''))
+addons = xbmc.translatePath(os.path.join('special://home/addons/', ''))
+resources = xbmc.translatePath(os.path.join('special://home/addons/plugin.video.palcotv/resources', ''))
+art = xbmc.translatePath(os.path.join('special://home/addons/plugin.video.palcotv/art', ''))
+tmp = xbmc.translatePath(os.path.join('special://home/addons/plugin.video.palcotv/tmp', ''))
+playlists = xbmc.translatePath(os.path.join('special://home/addons/playlists', ''))
+
+icon = art + 'icon.png'
+fanart = 'fanart.jpg'
+
+
+
+# En construcción!
+def castalba(params):
+    plugintools.log("[PalcoTV-0.3.0].castalba "+repr(params))
+
+    url = params.get("url")
+    plugintools.play_resolved_url(url)
